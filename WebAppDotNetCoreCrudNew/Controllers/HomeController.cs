f2cad68f622b349af63d7502318e7f6930d42b40
shohag66 <professional.cse66@gmail.com>
2022-07-30 00:19:12 +0600

    SomeInformationAdd

--- WebAppDotNetCoreCrudNew/Controllers/HomeController.cs
+++ WebAppDotNetCoreCrudNew/Controllers/HomeController.cs
@@ -35,3 +35,53 @@ namespace WebAppDotNetCoreCrudNew.Controllers
         }
     }
 }
+
+
+
+
+//"ConnectionStrings": {
+//    "DefaultConnection": "Server=DESKTOP-DLB3GM6;Database=InventoryManagement;Trusted_Connection=True; MultipleActiveResultSets=true"
+
+//  },
+
+//public class ApplicationDbContext : IdentityDbContext
+//{
+//    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+//       : base(options)
+//    {
+//    }
+
+//    public DbSet<Customer> Customers { get; set; }
+//    public DbSet<Product> Products { get; set; }
+//    public DbSet<Category> Categories { get; set; }
+//    public DbSet<Order> Orders { get; set; }
+//    public DbSet<OrderDetail> OrderDetails { get; set; }
+//    public DbQuery<VWOrderInfo> VwOrderInfos { get; set; }
+//}
+
+
+//{
+
+
+//    public void ConfigureServices(IServiceCollection services)
+//    {
+
+//        services.AddDbContext<ApplicationDbContext>(options =>
+//        options.UseSqlServer(
+//            Configuration.GetConnectionString("DefaultConnection")));
+
+//        services.AddTransient<ICustomerService, CustomerService>();
+//        services.AddTransient<ICustomerRepository, CustomerRepository>();
+
+//        services.AddTransient<ICategoryService, CategoryService>();
+//        services.AddTransient<ICategoryRepository, CategoryRepository>(
+//        services.AddAutoMapper();
+
+//        services.AddControllersWithViews();
+//    }
+
+
+//    Microsoft.EntityFrameworkCore.Design(2.2.0)
+//Microsoft.EntityFrameworkCore.SqlServer(2.2.0)
+//Microsoft.EntityFrameworkCore.Tools(2.2.0)
+//Microsoft.EntityFrameworkCore.SqlServer.Design(1.1.6)
