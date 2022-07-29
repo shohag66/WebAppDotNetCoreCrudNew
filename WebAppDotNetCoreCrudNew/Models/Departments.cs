39cdec097be39d3de485bfefc9f0e02b578672fa
shohag66 <professional.cse66@gmail.com>
2022-07-30 00:07:48 +0600

    Add project files.

---
+++ WebAppDotNetCoreCrudNew/Models/Departments.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppDotNetCoreCrudNew.Models
+{
+    public class Departments
+    {
+        [Key]
+        public int ID { get; set; }
+
+        public string Department { get; set; }
+
+    }
+}
